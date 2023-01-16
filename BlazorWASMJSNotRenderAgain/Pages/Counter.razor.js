5cbc3b8e0a777733ecb6c1175add2525936ead6a
Laftek <balintmat@gmail.com>
2023-01-15 16:42:28 -0800

    rive re-render and js function can be triggered but there is memory leak

--- BlazorWASMJSNotRenderAgain/Pages/Counter.razor.js
+++ BlazorWASMJSNotRenderAgain/Pages/Counter.razor.js
@@ -1,10 +1,52 @@
-﻿export function createRive() {
-    const r = new rive.Rive({
+﻿//import 'https://unpkg.com/@rive-app/canvas@1.0.79'; will not work because it needs to load before render
+
+
+let r;
+export function createRive() {
+    r = new rive.Rive({
         src: 'bear.riv',
         canvas: document.getElementById('canvas'),
         autoplay: true,
+        stateMachines: 'Login Machine',
+        fit: rive.Fit.cover,
     });
+    return r
 }
+
+export function doSuccessEmotionJs() {
+    const inputs = r.stateMachineInputs('Login Machine');
+    const bumpTrigger = inputs.find(i => i.name === 'trigSuccess');
+    bumpTrigger.fire();
+}
+
+//export function createRive() {
+//    const r = new rive.Rive({
+//        src: 'bear.riv',
+//        canvas: document.getElementById('canvas'),
+//        autoplay: true,
+//        stateMachines: 'Login Machine',
+//        fit: rive.Fit.cover,
+
+//        //onLoad: (_) => {
+//        //    const inputs = r.stateMachineInputs('Login Machine');
+//        //    const bumpTrigger = inputs.find(i => i.name === 'trigSuccess');
+//        //    bumpTrigger.fire();
+//        //    btn.onclick = (e) => {
+//        //        e.preventDefault();
+//        //        bumpTrigger.fire()
+//        //    };
+//        //            }
+
+//    })
+//}
+
+//const r = createRive();
+
+//export function doSuccessEmotionJs() {
+//    const inputs = r.stateMachineInputs('Login Machine');
+//    const bumpTrigger = inputs.find(i => i.name === 'trigSuccess');
+//    bumpTrigger.fire();
+//}
 
 
 

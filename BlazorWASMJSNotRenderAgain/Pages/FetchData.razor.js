ed06c7e502e0c1de598193ee3375a40cca1fcfb8
Laftek <balintmat@gmail.com>
2023-01-16 21:42:50 -0800

    adding rating star system with control from C# to javascript and vice versa

---
+++ BlazorWASMJSNotRenderAgain/Pages/FetchData.razor.js
@@ -0,0 +1,32 @@
+﻿//import 'https://unpkg.com/@rive-app/canvas@1.0.79'; will not work because it needs to load before render
+
+let r;
+export function createRive() {
+    r = new rive.Rive({
+        src: 'starRating.riv',
+        canvas: document.getElementById('canvas'),
+        autoplay: true,
+        stateMachines: 'State Machine 1',
+        fit: rive.Fit.cover,
+    });
+    return r
+}
+
+export function doStarsRatingFromNET(starsValue) {
+    const inputs = r.stateMachineInputs('State Machine 1');
+    const stars = inputs.find(i => i.name === 'rating');
+    if (stars) {
+        stars.value = starsValue;
+        //return stars.value;
+    }
+}
+
+export function doStarsRatingFromRive(starsValue) {
+    const inputs = r.stateMachineInputs('State Machine 1');
+    const stars = inputs.find(i => i.name === 'rating');
+    if (stars) {
+        //stars.value = starsValue;
+        return stars.value;
+    }
+}
+
